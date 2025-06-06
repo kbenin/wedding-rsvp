dcb26cea2e4e9d3cfb447515a82ddae6dbcf18ee
kbenin <kbenin24@gmail.com>
2025-06-07 03:43:17 +0800

    update index.php v2

--- api/index.php
+++ api/index.php
@@ -2,15 +2,15 @@
 
 require 'functions.php';
 require 'router.php';
-require 'Database.php';
+// require 'Database.php';
 
-$config = require('config.php');
-$db = new Database($config['database']);
+// $config = require('config.php');
+// $db = new Database($config['database']);
 
-$id = $_GET['id'];
-$query = "select * from posts where id = ?";
+// $id = $_GET['id'];
+// $query = "select * from posts where id = ?";
 
-$posts = $db->query($query, [$id])->fetch();
+// $posts = $db->query($query, [$id])->fetch();
  
 // dd($posts);
 
